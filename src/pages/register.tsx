e18ee24a47203c25ef94cf12646c5935dc2ba1ee
GitDevla <pataki.david.ferenc@gmail.com>
2023-03-01 15:20:01 +0100

    Add register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,26 +1,29 @@
+import { validateUserRegister } from '@/validator/userValidator';
 import { useRouter } from 'next/router';
 import React from 'react'
 import { useState } from 'react'
 
-function LoginPage() {
+function Register() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
     const [errorMessage, setErrorMessage] = useState('');
     const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
-            const response = await fetch('/api/auth', {
+            validateUserRegister({ username, password, email })
+            const response = await fetch('/api/user', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username, password }),
+                body: JSON.stringify({ username, password, email }),
             });
             const json = await response.json();
 
             if (!response.ok)
                 throw new Error(json.error);
-            router.push('/home');
+            router.push('/login');
         } catch (error) {
             if (error instanceof Error)
                 setErrorMessage(error.message);
@@ -37,14 +40,19 @@ function LoginPage() {
             </label>
             <br />
             <label>
+                Email:
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <br />
+            <label>
                 Jelszó:
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </label>
             <br />
-            <button type="submit">Belépés</button>
+            <button type="submit">Regisztráció</button>
             {errorMessage && <p>{errorMessage}</p>}
         </form>
     )
 }
 
-export default LoginPage
+export default Register
