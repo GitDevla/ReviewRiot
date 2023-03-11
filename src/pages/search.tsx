5473d531c2dc3bed0fe17ff6cbeb285778c521aa
GitDevla <pataki.david.ferenc@gmail.com>
2023-03-11 19:32:10 +0100

    Create temporary search api

---
+++ src/pages/search.tsx
@@ -0,0 +1,44 @@
+import Layout from '@/component/Layout';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { Fetch } from '@/util/Fetch';
+
+function Search() {
+    const [result, setResult] = useState([] as { id: number, name: string, picture: string, type: string }[])
+    const [inputValue, setInputValue] = useState("")
+    const timeoutId = useRef(null as NodeJS.Timeout | null);
+    const cd = 200;
+
+    async function query() {
+        const res = await Fetch.GET("/api/search?name=" + inputValue);
+        setResult(await res.json());
+        console.log(result);
+
+    }
+
+    useEffect(() => {
+        clearTimeout(timeoutId.current!);
+        const newTimeoutId = setTimeout(() => {
+            query()
+
+        }, cd);
+        timeoutId.current = newTimeoutId;
+    }, [inputValue]);
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        setInputValue(event.target.value);
+    }
+    return (
+        <Layout>
+            <div id='feed'>
+                <input type="text" value={inputValue} onChange={handleInputChange} />
+                {result.map((i, id) => {
+                    return <div key={id}>
+                        <p>{i.type}:{i.name}</p>
+                    </div>
+                })}
+            </div>
+        </Layout>
+    )
+}
+
+export default Search;
