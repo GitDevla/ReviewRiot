0f1129fb4857dfdbff9051fb81d5278e583b5e06
GitDevla <pataki.david.ferenc@gmail.com>
2023-03-23 16:14:35 +0100

    Wowers thank you next js devs <33333333

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const server = express()
+
+    if (!dev)
+        server.use("/", express.static(__dirname + "/public"));
+
+    server.all('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+        if (err) throw err
+        console.log(`ready - started server on 0.0.0.0:${port}, url: http://localhost:${port}`)
+    })
+})
